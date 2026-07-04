d2dac3dd97fc1787bc9cf6cdc6f3e7c2f68f2f6d
nihiL7331 <pujanekp6@gmail.com>
2026-07-04 16:53:31 +0200

    feat: add clips fetch bash script

---
+++ fetch_test_cases.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+SETTINGS="bestvideo[ext=mp4]+bestaudio[ext=m4a]/best"
+VIDEO_URL="https://www.youtube.com/watch?v=FYkRCafLCiQ"
+CLIP_DIR="clips"
+
+mkdir -p "$CLIP_DIR"
+
+if ! command -v yt-dlp &> /dev/null; then
+  echo "yt-dlp is required to grab YT clips but it isn't installed."
+  echo "Install it using your package manager."
+  exit 1
+fi
+
+if ! command -v ffmpeg &> /dev/null; then
+  echo "ffmpeg is required to slice YT streams but it isn't installed."
+  echo "Install it using your package manager."
+  exit 1
+fi
+
+yt-dlp -f "$SETTINGS" --download-sections "*00:04:00-00:04:45" -o "$CLIP_DIR/01.mp4" "$VIDEO_URL"
+yt-dlp -f "$SETTINGS" --download-sections "*00:05:00-00:05:45" -o "$CLIP_DIR/02.mp4" "$VIDEO_URL"
+yt-dlp -f "$SETTINGS" --download-sections "*00:22:00-00:22:45" -o "$CLIP_DIR/03.mp4" "$VIDEO_URL"
+yt-dlp -f "$SETTINGS" --download-sections "*00:36:00-00:36:45" -o "$CLIP_DIR/04.mp4" "$VIDEO_URL"
+yt-dlp -f "$SETTINGS" --download-sections "*00:41:00-00:41:45" -o "$CLIP_DIR/05.mp4" "$VIDEO_URL"
+
+echo "All clips downloaded to '$CLIP_DIR/' directory."
